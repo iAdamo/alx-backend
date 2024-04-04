b805a7c4f1b3d67fb82724c7eefa5091070a7d13
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-04-04 06:00:02 +0100

    LFU Caching

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""LFU caching
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """LFU cache
+    """
+
+    def __init__(self):
+        """Constructor
+        """
+        super().__init__()
+        self.lfu_keys = {}
+
+    def put(self, key, item):
+        """Add an item in the cache
+        """
+        if key and item and self.cache_data.get(key) != item:
+
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                k = min(self.lfu_keys, key=self.lfu_keys.get)
+                self.cache_data.pop(k)
+                self.lfu_keys.pop(k)
+                print(f"DISCARD: {k}")
+
+            self.lfu_keys[key] = self.lfu_keys.get(key, 0) + 1
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.lfu_keys:
+            self.lfu_keys[key] += 1
+        return self.cache_data.get(key)
